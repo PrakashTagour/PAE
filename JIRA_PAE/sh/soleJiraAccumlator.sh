4196b9a25b863c41470317403d2acba569d2f0d1
Prakash <prakash.tagour@footlocker.com>
2024-12-24 12:25:52 -0600

    rename files

--- JIRA_PAE/sh/soleJiraAccumlator.sh
+++ JIRA_PAE/sh/soleJiraAccumlator.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /Users/u1002018/development/bin/activate  
-python /Users/u1002018/src/PAE/JIRA_PAE/py/sole_proj.py SOLE
+python /Users/u1002018/src/PAE/JIRA_PAE/py/JiiraAccumulator.py SOLE
 deactivate
